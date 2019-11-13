f2d23b36478cc9fa0648ac3c9c60001ea23171fa
adminyh <1658431245@qq.com>
2019-11-13 22:20:33 +0800

    login and register

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpringBoot-mybatis
 SpringBoot-mybatis implements the back-end
+
+前后端分离，后端实现登录注册功能
---
+++ src/main/java/com/nuaa/travel/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.nuaa.travel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "this is home";
+    }
+}
